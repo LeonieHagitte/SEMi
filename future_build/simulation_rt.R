a4f0916b37741b4a64ee9af5b28e6eccfc87d864
brandmaier <brandmaier@mpib-berlin.mpg.de>
2026-05-13 15:25:10 +0200

    added count of workers

--- future_build/simulation_rt.R
+++ future_build/simulation_rt.R
@@ -527,7 +527,7 @@ DESIGN <- DESIGN %>%
 #############################################
 
 plan(list(
-  tweak(multisession, workers = parallelly::availableWorkers() - 1),
+  tweak(multisession, workers = length(parallelly::availableWorkers()) - 1),
   sequential
 ))
 
