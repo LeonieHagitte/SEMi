da748011c348870e00a9653575158ac798fec39d
brandmaier <brandmaier@mpib-berlin.mpg.de>
2026-05-13 15:31:30 +0200

    revised SLURM script

--- future_build/submit_jobs.sh
+++ future_build/submit_jobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=mnlfa_trees_sim
 #SBATCH --array=1-100
-#SBATCH --time=02:00:00
+#SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --mem=2G
 #SBATCH --output=logs/slurm_%A_%a.out
 #SBATCH --error=logs/slurm_%A_%a.err
 
