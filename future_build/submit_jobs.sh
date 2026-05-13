a0d14400b0e54a335de1acb1f942917f616d1b19
brandmaier <brandmaier@mpib-berlin.mpg.de>
2026-05-13 13:47:26 +0200

    added SLURM

---
+++ future_build/submit_jobs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=mnlfa_trees_sim
+#SBATCH --array=1-100
+#SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --output=logs/slurm_%A_%a.out
+#SBATCH --error=logs/slurm_%A_%a.err
+
+mkdir logs
+Rscript simulation_rt.R ${SLURM_ARRAY_TASK_ID} 100
