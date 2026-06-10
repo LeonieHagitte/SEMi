20147fe3d52e7344deb10ad7b834c08b3576e1e8
brandmaier <brandmaier@mpib-berlin.mpg.de>
2026-06-10 09:35:58 +0200

    run 1000 jobs in the array with 24h run time

--- future_build/submit_jobs.sh
+++ future_build/submit_jobs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=mnlfa_trees_sim
-#SBATCH --array=1-100
-#SBATCH --time=12:00:00
+#SBATCH --array=1-1000
+#SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=2G
 #SBATCH --output=logs/slurm_%A_%a.out
 #SBATCH --error=logs/slurm_%A_%a.err
 
 mkdir logs
-Rscript simulation_rt.R ${SLURM_ARRAY_TASK_ID} 100
+Rscript simulation_rt.R ${SLURM_ARRAY_TASK_ID} 1000
